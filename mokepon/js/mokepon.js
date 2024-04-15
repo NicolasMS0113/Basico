6de241754895f6c4865c24fa4df802261e1fa43f
Nicolas <nicolasms0113@gmail.com>
2024-04-14 22:21:19 -0500

    Juego terminado sin css

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,7 +1,15 @@
 let ataqueJugador
 let ataqueEnemigo
+let vidasEnemigo = 3
+let vidasJugador = 3
 
 function iniciarJuego(){
+    let sectionSeleccionarMascota= document.getElementById("seleccionar-ataque")
+    sectionSeleccionarMascota.style.display ='none'
+
+    let sectionReiniciar =document.getElementById("Reiniciar")
+    sectionReiniciar.style.display="none"
+
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)    
 
@@ -11,10 +19,23 @@ function iniciarJuego(){
     botonAgua.addEventListener('click',ataqueAgua)
     let botonTierra = document.getElementById("boton-tierra")
     botonTierra.addEventListener('click',ataqueTierra)
+
+    let botonReiniciar = document.getElementById("boton-reiniciar")
+    botonReiniciar.addEventListener('click',reiniciarJuego)
 }
 
 
 function seleccionarMascotaJugador(){
+    let sectionSeleccionarMascota= document.getElementById("seleccionar-ataque")
+    sectionSeleccionarMascota.style.display ='block'
+
+    let sectionReiniciar =document.getElementById("Reiniciar")
+    sectionReiniciar.style.display="none"
+
+    let sectionSeleccionarAtaque= document.getElementById("seleccionar-mascota")
+    sectionSeleccionarAtaque.style.display ='none'
+    
+    
     let inputHipodoge = document.getElementById('Hipodoge')
     let inputCapipepo = document.getElementById('Capipepo')
     let inputRatigueya= document.getElementById('Ratigueya')
@@ -69,13 +90,76 @@ function ataqueAleatorioEnemigo (){
     } else if (ataqueAleatorio==3){
         ataqueEnemigo= "TIERRA"
     }
+    combate()
 }
 
 function aleatorio(min,max) {
       return Math.floor(Math.random()*(max-min+1)+min) 
 }
 
+function combate () {
+    //combate
+    let spanVidasJugador = document.getElementById("vidas-jugador")
+    let spanVidasEnemigo = document.getElementById("vidas-enemigo")
 
+    if (ataqueEnemigo == ataqueJugador){
+        crearMensaje("EMPATE")
+    } else if (ataqueJugador== "FUEGO" && ataqueEnemigo=="TIERRA" ||ataqueJugador=="AGUA" && ataqueEnemigo=="FUEGO" || ataqueJugador=="TIERRA" && ataqueEnemigo=="AGUA" ) {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else {
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+    revisarVidas()
+}
+
+function revisarVidas() {
+    if (vidasEnemigo==0){
+        crearMensajeFinal("Felicitaciones Ganaste")
+    } else if (vidasJugador==0){
+        crearMensajeFinal("Perdiste :(")
+    }
+
+
+
+}
+
+
+function crearMensaje(resultado) {
+    let sectionMensajes = document.getElementById("Mensajes")//apuntamos la id de donde queremos que aparezca nuestro mensaje
+
+    let parrafo = document.createElement('p')//creamos el elemento al cual llegara nuestro mensaje
+    parrafo.innerHTML = "tu mascota ataco con " + ataqueJugador + ", la mascota del enemigo ataco con " + ataqueEnemigo + " - " + resultado // al "p" le insertamos el texto 
+
+    sectionMensajes.appendChild(parrafo) // al id le decimos con appendchild que le vamos a insertar en este caso parrafo, que ya le insetamos el mensaje y se sabe que es de tipo "p"
+}
+
+function crearMensajeFinal(resultadoFinal) {
+    let sectionMensajes = document.getElementById("Mensajes")
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensajes.appendChild(parrafo) 
+
+    
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.disabled=true
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.disabled=true
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.disabled=true
+
+    let sectionReiniciar =document.getElementById("Reiniciar")
+    sectionReiniciar.style.display="block"
+}
+
+function reiniciarJuego (){
+    location.reload()
+}
 
 
 window.addEventListener('load',iniciarJuego)
